7d218b5d51dc11cefd090bb9c53619113b40c914
FelixAlcor <Felix@assert.de>
2025-05-04 18:52:18 +0200

    - fixed

--- obj/Debug/net6.0/MyPluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace SleepyDice
-{
-    public static class MyPluginInfo
-    {
-        public const string PLUGIN_GUID = "SleepyDice";
-        public const string PLUGIN_NAME = "SleepyDice";
-        public const string PLUGIN_VERSION = "0.1.10";
-    }
-}
